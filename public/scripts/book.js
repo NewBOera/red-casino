8f66c702f37b4be1c91ea1a685c6841152063c83
Álvaro Torres <alvaro.espindola.torres@gmail.com>
2024-09-04 22:09:22 -0600

    feat: add booking methods

--- public/scripts/book.js
+++ public/scripts/book.js
@@ -1,83 +1,29 @@
-const hotels = [
-  'DoubleTree by Hilton Coventry Building Society Arena',
-  'Prestige Plaza Hotel',
-  'Zedwell Piccadilly Circus',
-  'St Giles Hotel London',
-  'The First Luxury Home',
-  'Genting Hotel',
-  'Aspect Apartments City Centre',
-];
-const urlParams = new URLSearchParams(window.location.search);
+function goToBookingForm() {
+  window.location.href = '#contact-section';
+}
 
-function showHotel(userHotel) {
-  if (!validateHotel(userHotel)) {
-    const main = document.querySelector('main');
-    main.classList.add('blur-3xl');
-    notify.error('Invalid URL parameters, please try later', 3000);
-    setTimeout(() => {
-      window.location.href = 'index.html';
-    }, 4000);
+function bookEvent(event) {
+  const eventsSelect = document.getElementById('event-select');
+  console.log(eventsSelect);
+
+  const events = eventsSelect.options;
+
+  disabledEventSelected(events);
+  for (let i = 0; i < events.length; i++) {
+    if (events[i].value === event) {
+      events[i].setAttribute('selected', 'selected');
+      break;
+    }
   }
 
-  dismissHotels(hotels);
-
-  hotelVisibilityHook(userHotel);
-  activeFormOption(userHotel);
+  goToBookingForm();
 }
 
-function getURLParams() {
-  try {
-    const userHotel = urlParams.get('hotel');
-    if (!userHotel) {
-      throw new Error('No hotel provided');
-    }
-
-    return userHotel;
-  } catch (error) {
-    notify.error(error.message, 3000);
-    return null;
-  }
-}
-
-function validateHotel(hotel) {
-  return hotels.includes(hotel);
-}
-
-function hotelVisibilityHook(hotel) {
-  const hotelElement = document.getElementById(hotel);
-  hotelElement.classList.toggle('hidden');
-}
-
-function dismissHotels(hotels) {
-  hotels.forEach(hotel => {
-    const hotelElement = document.getElementById(hotel);
-    console.log(hotelElement);
-
-    hotelElement.classList.add('hidden');
-  });
-}
-
-function activeFormOption(hotelSelected) {
-  const selectHotels = document.getElementById('selectHotels');
-  const options = selectHotels.options;
-
-  for (let i = 0; i < options.length; i++) {
-    if (options[i].value !== hotelSelected && options[i].selected) {
-      options[i].selected = false;
-    }
-
-    if (options[i].value === hotelSelected && !options[i].selected) {
-      options[i].selected = true;
+function disabledEventSelected(events) {
+  for (let i = 0; i < events.length; i++) {
+    if (events[i].getAttribute('selected')) {
+      events[i].removeAttribute('selected');
+      return;
     }
   }
 }
-
-function addSelectListener() {
-  const selectHotels = document.getElementById('selectHotels');
-  selectHotels.addEventListener('change', function () {
-    const selectedHotel = this.value;
-    showHotel(selectedHotel);
-  });
-}
-
-window.onload = addSelectListener();
